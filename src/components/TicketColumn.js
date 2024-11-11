23e68d079a044f246b198924dc2efea96d03741e
Ajay Singh <ajaysingh.dev18@gmail.com>
2024-11-11 22:06:05 +0100

    Added add and menu icons

--- src/components/TicketColumn.js
+++ src/components/TicketColumn.js
@@ -7,9 +7,13 @@ const TicketColumn = ({ title, icon, tickets }) => {
   return (
     <div className="ticket-column">
       <div className="title">
-        {icon && <img className="title-img" src={icon} alt={title} />}
+        {icon && <img src={icon} alt={title} />}
         <h2>{title}</h2>
         <span className="ticket-count">{tickets.length}</span>
+        <div className="actions">
+          <img src="/icons/add.svg" alt="add" />
+          <img src="/icons/3 dot menu.svg" alt="menu" />
+        </div>
       </div>
       {tickets.map((ticket) => (
         <Ticket key={ticket.id} ticket={ticket} />
